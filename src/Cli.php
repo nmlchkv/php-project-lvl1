4536711b914d566d4c6005a0f3a8bd5acd5e682a
Nikolai Malchikov <malchikov.ne@gmail.com>
2022-02-09 13:34:26 +0300

    update project

---
+++ src/Cli.php
@@ -0,0 +1,9 @@
+<?php
+namespace Php\Package;
+
+  use function cli\line;
+  use function cli\prompt;
+
+  line('Welcome to the Brain Game!');
+  $name = prompt('May I have your name?');
+  line("Hello, %s!", $name);
