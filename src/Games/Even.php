83af4026b3291655d7d2df21065108d40717b74c
itaopro <malchikov.ne@gmail.com>
2022-02-16 14:22:02 +0300

    update

---
+++ src/Games/Even.php
@@ -0,0 +1,25 @@
+<?php
+namespace src\games\even;
+use function cli\line;
+use function cli\prompt;
+
+line('Answer "yes" if the number is even, otherwise answer "no".');
+        $num = rand(1, 15);
+        line('Question: ' . $num);
+        $question = prompt('Your answer');
+if ($num % 2 === 0 && $question === 'yes' || $num % 2 !== 0 && $question === 'no') {
+                line('Correct!');
+                $num = rand(1, 15);
+                line('Question: ' . $num);
+                $question = prompt('Your answer');
+} if ($num % 2 === 0 && $question === 'yes' || $num % 2 !== 0 && $question === 'no') {
+                line('Correct!');
+                $num = rand(1, 15);
+                line('Question: ' . $num);
+                $question = prompt('Your answer');
+} if ($num % 2 === 0 && $question === 'yes' || $num % 2 !== 0 && $question === 'no') {
+                line('Congratulations, %s!', $name);
+} else {
+        line("'yes' is wrong answer ;(. Correct answer was 'no'.
+        Let's try again, %s!", $name);
+}
