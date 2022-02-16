0bf797ca875518cc1ea70bf3c0463a8922b8e255
itaopro <malchikov.ne@gmail.com>
2022-02-16 17:16:34 +0300

    update

--- src/Games/Even.php
+++ src/Games/Even.php
@@ -5,7 +5,8 @@ namespace src\games\even;
 use function cli\line;
 use function cli\prompt;
 use function src\engine;
-
+function operacion () 
+{
 line('Welcome to the Brain Game!');
 $name = prompt('May I have your name?');
 line("Hello, %s!", $name);
@@ -28,4 +29,5 @@ if ($num % 2 === 0 && $question === 'yes' || $num % 2 !== 0 && $question === 'no
 } else {
         line("'yes' is wrong answer ;(. Correct answer was 'no'.
         Let's try again, %s!", $name);
+}
 }
