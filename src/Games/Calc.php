769a40d8d6fa2671c8ffad97924f1e990290c654
itaopro <malchikov.ne@gmail.com>
2022-02-16 16:41:05 +0300

    update

--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -14,6 +14,7 @@ use function src\engine;
     $num2 = rand(1, 10);
     line('Question: ' . $num1 . ' ' . $operator . ' ' . $num2);
     $question = prompt('Your answer');
+    $user = new src\engine($name);
 switch ($operator) {
     case '+':
         $answer =  $num1 + $num2;
