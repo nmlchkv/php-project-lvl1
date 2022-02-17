f50c7a14772a2a8ab17737818207c075abb9a1f5
itaopro <malchikov.ne@gmail.com>
2022-02-17 10:55:22 +0300

    update

---
+++ src/Games/Gcd.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace src\games\gcd;
+
+use function cli\line;
+use function cli\prompt;
+use function src\engine;
+function operacion ()
+{
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    line('Find the greatest common divisor of given numbers.');
+    $num1 = rand(1, 10);
+    $num2 = rand(1, 10);
+    line('Question: ' . $num1 . ' ' . $num2);
+    $question = prompt('Your answer');
+}
