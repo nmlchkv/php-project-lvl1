5c6021837abf0f456a150983ac6a5ecc6fdb3616
itaopro <malchikov.ne@gmail.com>
2022-02-17 13:31:30 +0300

    update

--- src/Games/Gcd.php
+++ src/Games/Gcd.php
@@ -5,7 +5,13 @@ namespace src\games\gcd;
 use function cli\line;
 use function cli\prompt;
 use function src\engine;
-public function operacion ()
+function gcd($num1, $num2) {
+    $num = ((($num1 % $num2) ? gcd($num2,$num1 % $num2) : $num2));
+    if ($num == $question) {
+        line('Correct!');
+    }
+}
+function operacion ()
 {
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
@@ -15,11 +21,5 @@ public function operacion ()
     $num2 = rand(1, 10);
     line('Question: ' . $num1 . ' ' . $num2);
     $question = prompt('Your answer');
-
+    $num = gcd($num1, $num2);
 } 
-function gcd($num1, $num2) {
-    $num = ((($num1 % $num2) ? gcd($num2,$num1 % $num2) : $num2));
-    if ($num == $question) {
-        line('Correct!');
-    }
-}
