2f69be8d3049ea5b132d8a4ae99bcf87563d80dd
itaopro <malchikov.ne@gmail.com>
2022-02-17 14:49:15 +0300

    update

--- src/Engine.php
+++ src/Engine.php
@@ -4,13 +4,14 @@
 
   use function cli\line;
   use function cli\prompt;
-  function welcome ()
-  {
+function welcome ()
+{
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
-  }
-     function gcd($num1, $num2) {
+}
+function gcd($num1, $num2) 
+{
       $num = ((($num1 % $num2) ? gcd($num2,$num1 % $num2) : $num2));
       return $num;
-  }
+}
