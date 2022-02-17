0dcfd1cfd8705ecf34c7dfa696ec78d8e62736c0
itaopro <malchikov.ne@gmail.com>
2022-02-17 10:56:47 +0300

    update

--- src/Cli.php
+++ src/Cli.php
@@ -3,9 +3,8 @@
   namespace src\cli;
 
   use function cli\line;
-  use function cli\prompt;
-
+  use function cli\prompt
+  
   line('Welcome to the Brain Game!');
   $name = prompt('May I have your name?');
   line("Hello, %s!", $name);
-  
--- src/Engine.php
+++ src/Engine.php
@@ -4,7 +4,9 @@
 
   use function cli\line;
   use function cli\prompt;
-  
-  line('Welcome to the Brain Game!');
-  $name = prompt('May I have your name?');
-  line("Hello, %s!", $name);
+  function welcome ()
+  {
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+     }
