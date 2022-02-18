c6d5e1f3b1109fa2cb475d0c0fb3c3c1c9adf211
itaopro <malchikov.ne@gmail.com>
2022-02-18 16:36:56 +0300

    update

--- src/Games/Progression.php
+++ src/Games/Progression.php
@@ -7,5 +7,16 @@ use function cli\prompt;
 
 function operacion ()
 {
-
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    line('What number is missing in the progression?');
+    $num1 = rand(1, 10);
+    $num2 = rand(1, 3);
+    $result = '';
+    for($i = 1; $i > $num1; $i++) {
+        $result = $i + $num2;
+    }
 }
+
+
