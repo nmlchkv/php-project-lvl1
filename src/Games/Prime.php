70f47d48abd67240cca450172b25236de378495d
itaopro <malchikov.ne@gmail.com>
2022-02-19 20:08:52 +0300

    update

--- src/Games/Prime.php
+++ src/Games/Prime.php
@@ -7,4 +7,27 @@ use function cli\prompt;
 
 function operacion()
 {
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    line('Answer "yes" if given number is prime. Otherwise answer "no".');
+    $rand = rand(1, 100);
+    line('Question: ' . $rand);
+    $question = prompt('Your answer');
+    if (gmp_prob_prime($rand)) {
+        line('Correct!');
+        $rand = rand(1, 100);
+        line('Question: ' . $rand);
+        $question = prompt('Your answer');
+        } if (gmp_prob_prime($rand)) {
+            line('Correct!');
+            $rand = rand(1, 100);
+            line('Question: ' . $rand);
+            $question = prompt('Your answer');
+        } if (gmp_prob_prime($rand)) {
+            line("Congratulations, $name!");
+        } else {
+            line("'yes' is wrong answer ;(. Correct answer was 'no'.
+        Let's try again, %s!", $name);
+        }
 }
