02dec89a8726c6f4f321032f8c793f9541708735
itaopro <malchikov.ne@gmail.com>
2022-02-19 20:23:01 +0300

    update

--- src/Games/Prime.php
+++ src/Games/Prime.php
@@ -14,17 +14,17 @@ function operacion()
     $rand = rand(1, 100);
     line('Question: ' . $rand);
     $question = prompt('Your answer');
-    if (gmp_prob_prime($rand)) {
+    if (gmp_prob_prime($rand) == 2 && $question === 'yes') {
         line('Correct!');
         $rand = rand(1, 100);
         line('Question: ' . $rand);
         $question = prompt('Your answer');
-        } if (gmp_prob_prime($rand)) {
+        } if (gmp_prob_prime($rand) == 2 && $question === 'yes') {
             line('Correct!');
             $rand = rand(1, 100);
             line('Question: ' . $rand);
             $question = prompt('Your answer');
-        } if (gmp_prob_prime($rand)) {
+        } if (gmp_prob_prime($rand) == 2 && $question === 'yes') {
             line("Congratulations, $name!");
         } else {
             line("'yes' is wrong answer ;(. Correct answer was 'no'.
