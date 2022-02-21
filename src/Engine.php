759251a11a883d760013d7ed6c5383b58d283bc4
itaopro <malchikov.ne@gmail.com>
2022-02-21 13:30:45 +0300

    update

--- src/Engine.php
+++ src/Engine.php
@@ -5,9 +5,9 @@
   use function cli\line;
   use function cli\prompt;
 
-  function logic()
-  {
+function logic()
+{
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
-  }
+}
