2d490f064087df27cdaa8e43856dcc7be4e15a6c
itaopro <48715986+itaopro@users.noreply.github.com>
2022-02-21 22:31:12 +0300

    Update Engine.php

--- src/Engine.php
+++ src/Engine.php
@@ -4,10 +4,3 @@
 
   use function cli\line;
   use function cli\prompt;
-
-function logic()
-{
-    line('Welcome to the Brain Game!');
-    $name = prompt('May I have your name?');
-    line("Hello, %s!", $name);
-}
