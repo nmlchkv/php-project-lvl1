6f404caa27f18da15a9382c43e1f4c071ad74f6c
itaopro <malchikov.ne@gmail.com>
2022-02-22 11:23:47 +0300

    update

--- src/Cli.php
+++ src/Cli.php
@@ -5,7 +5,7 @@
   use function cli\line;
   use function cli\prompt
 
-  function operacion()
+function operacion()
 {
   line('Welcome to the Brain Game!');
   $name = prompt('May I have your name?');
