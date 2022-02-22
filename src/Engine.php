ba27831345e7f94786eb96765c3ac393c6b5f3f8
itaopro <malchikov.ne@gmail.com>
2022-02-22 13:12:54 +0300

    гзвфеу

--- src/Engine.php
+++ src/Engine.php
@@ -11,18 +11,18 @@
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
     line($question);
-    for($i = 0; $i <= count($array) - 1; $i++) {
-      $str = implode(' ', $array[$i]);
-      $strRand = implode(' ', $arrayRand[$i]);
-      line('Question: ' . $str);
-      $answer = prompt('Your answer');
-        if($strRand == $answer) {
-          line('Correct!');
-      } else {
-        line("'{$answer}' is wrong answer ;(. Correct answer was '{$strRand}'.
-        Let's try again, $name!");
-        break;
-     }
-     line("Congratulations, $name!");
+    for ($i = 0; $i <= count($array) - 1; $i++) {
+        $str = implode(' ', $array[$i]);
+        $strRand = implode(' ', $arrayRand[$i]);
+        line('Question: ' . $str);
+        $answer = prompt('Your answer');
+        if ($strRand == $answer) {
+            line('Correct!');
+        } else {
+            line("'{$answer}' is wrong answer ;(. Correct answer was '{$strRand}'.
+            Let's try again, $name!");
+            break;
+      }
+        line("Congratulations, $name!");
   }
 }
