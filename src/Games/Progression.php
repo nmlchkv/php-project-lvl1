417a8a64b7309655aa8bd07b9c0579c80d7a6bc0
itaopro <malchikov.ne@gmail.com>
2022-02-22 11:51:18 +0300

    update

--- src/Games/Progression.php
+++ src/Games/Progression.php
@@ -20,6 +20,7 @@ function operacion()
     $rand = rand(0, $count);
     $randStr = $result[$rand];
     $number = str_replace($randStr, '...', $str);
-    return  (logic($number, $question));
+    return  (($number, $question));
       
 }
+logic($number, $question);
