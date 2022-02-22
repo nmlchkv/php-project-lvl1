8c5e87dd41074af970e2bf45048dafe68184bb3f
itaopro <malchikov.ne@gmail.com>
2022-02-22 12:35:09 +0300

    update

--- src/Engine.php
+++ src/Engine.php
@@ -5,7 +5,7 @@
   use function cli\line;
   use function cli\prompt;
 
-  function logic($number, $question)
+  function logic($array, $question)
   {
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
@@ -13,11 +13,11 @@
     line($question);
     $answer = prompt('Your answer');
     for($i = 0; $i <= 2; $i++) {
-      line('Question: ' . $number);
-      if($number == $answer) {
+      line('Question: ' . $array[$i]);
+      if($array[$i] == $answer) {
         line('Correct!')
       } else {
-        line("'{$answer}' is wrong answer ;(. Correct answer was '{$number}'.
+        line("'{$answer}' is wrong answer ;(. Correct answer was '{$array[$i]}'.
         Let's try again, $name!");
     }
   }
