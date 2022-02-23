3fba74036e03190c9e11395a0bc301df2b2dda1a
itaopro <malchikov.ne@gmail.com>
2022-02-23 11:43:44 +0300

    update

--- src/Games/Even.php
+++ src/Games/Even.php
@@ -4,31 +4,22 @@ namespace src\games\even;
 
 use function cli\line;
 use function cli\prompt;
-use function src\engine;
+use function src\engine\logic;
 
 function operacion()
 {
-        line('Welcome to the Brain Game!');
-        $name = prompt('May I have your name?');
-        line("Hello, %s!", $name);
-        line('Answer "yes" if the number is even, otherwise answer "no".');
+    $question = ('Answer "yes" if the number is even, otherwise answer "no".');
+    $arrayRand = [];
+    $array = [];
+    for($i = 0; $i <= 2; $i++) {
         $num = rand(1, 15);
-        line('Question: ' . $num);
-        $question = prompt('Your answer');
-    if ($num % 2 === 0 && $question === 'yes' || $num % 2 !== 0 && $question === 'no') {
-                line('Correct!');
-                $num = rand(1, 15);
-                line('Question: ' . $num);
-                $question = prompt('Your answer');
-    } if ($num % 2 === 0 && $question === 'yes' || $num % 2 !== 0 && $question === 'no') {
-                line('Correct!');
-                $num = rand(1, 15);
-                line('Question: ' . $num);
-                $question = prompt('Your answer');
-    } if ($num % 2 === 0 && $question === 'yes' || $num % 2 !== 0 && $question === 'no') {
-                line('Congratulations, %s!', $name);
-    } else {
-        line("'yes' is wrong answer ;(. Correct answer was 'no'.
-        Let's try again, %s!", $name);
-    }
+        $array[] = [$num];
+        if ($num % 2 === 0) {
+            $arrayRand[] = ['yes'];
+        } else {
+            $arrayRand[] = ['no'];
+        }
+    $logic = logic($array, $question, $arrayRand);
+    return $logic;
+}
 }
