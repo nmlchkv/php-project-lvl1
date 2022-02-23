420a1fda023c4ab04d7d0d05296c48f3fee2e1ea
itaopro <malchikov.ne@gmail.com>
2022-02-23 12:34:07 +0300

    update

--- src/Games/Gcd.php
+++ src/Games/Gcd.php
@@ -7,34 +7,15 @@ use function cli\prompt;
 
 function operacion()
 {
-    line('Welcome to the Brain Game!');
-    $name = prompt('May I have your name?');
-    line("Hello, %s!", $name);
-    line('Find the greatest common divisor of given numbers.');
-    $num1 = rand(1, 10);
-    $num2 = rand(1, 10);
-    line('Question: ' . $num1 . ' ' . $num2);
-    $question = prompt('Your answer');
-    $num = gcd($num1, $num2);
-    if ($num == $question) {
-        line('Correct!');
+    $question = ('Find the greatest common divisor of given numbers.');
+    $arrayRand = [];
+    $array = [];
+    for ($i = 0; $i <= 2; $i++) {
         $num1 = rand(1, 10);
         $num2 = rand(1, 10);
-        line('Question: ' . $num1 . ' ' . $num2);
-        $question = prompt('Your answer');
+        $array[] = ["$num1 $num2"];
         $num = gcd($num1, $num2);
-    } if ($num == $question) {
-        line('Correct!');
-        $num1 = rand(1, 10);
-        $num2 = rand(1, 10);
-        line('Question: ' . $num1 . ' ' . $num2);
-        $question = prompt('Your answer');
-        $num = gcd($num1, $num2);
-    } if ($num == $question) {
-        line("Congratulations, $name!");
-    } else {
-        line("'$question' is wrong answer ;(. Correct answer was '$num'.
-        Let's try again, $name!");
+        $arrayRand[] = [$num];
     }
 }
 function gcd($num1, $num2)
