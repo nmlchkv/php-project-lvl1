77a7d00a6defed71a3e823789a1f0653597063b3
itaopro <malchikov.ne@gmail.com>
2022-02-23 11:55:52 +0300

    update

--- src/Games/Prime.php
+++ src/Games/Prime.php
@@ -7,27 +7,19 @@ use function cli\prompt;
 
 function operacion()
 {
-    line('Welcome to the Brain Game!');
-    $name = prompt('May I have your name?');
-    line("Hello, %s!", $name);
-    line('Answer "yes" if given number is prime. Otherwise answer "no".');
-    $rand = rand(1, 100);
-    line('Question: ' . $rand);
-    $question = prompt('Your answer');
-    if (gmp_prob_prime($rand) === 2 && $question === 'yes' || gmp_prob_prime($rand) !== 2 && $question === 'no') {
-        line('Correct!');
-        $rand = rand(1, 100);
-        line('Question: ' . $rand);
-        $question = prompt('Your answer');
-    } if (gmp_prob_prime($rand) === 2 && $question === 'yes' || gmp_prob_prime($rand) !== 2 && $question === 'no') {
-            line('Correct!');
-            $rand = rand(1, 100);
-            line('Question: ' . $rand);
-            $question = prompt('Your answer');
-    } if (gmp_prob_prime($rand) === 2 && $question === 'yes' || gmp_prob_prime($rand) !== 2 && $question === 'no') {
-            line("Congratulations, $name!");
-    } else {
-            line("'yes' is wrong answer ;(. Correct answer was 'no'.
-        Let's try again, %s!", $name);
+    $question = ('Answer "yes" if given number is prime. Otherwise answer "no".');
+    $arrayRand = [];
+    $array = [];
+    for ($i = 0; $i <= 2; $i++) {
+        $num = rand(1, 100);
+        $array[] = [$num];
+        if (gmp_prob_prime($rand) === 2) {
+            $arrayRand[] = ['yes'];
+        } else {
+            $arrayRand[] = ['no'];
+        }
     }
+    $logic = logic($array, $question, $arrayRand);
+    return $logic;
+}
 }
