b0acea3c1ef51b60a06f99342d6d2a87956dbf9e
itaopro <malchikov.ne@gmail.com>
2022-02-23 11:50:26 +0300

    update

--- src/Games/Even.php
+++ src/Games/Even.php
@@ -11,7 +11,7 @@ function operacion()
     $question = ('Answer "yes" if the number is even, otherwise answer "no".');
     $arrayRand = [];
     $array = [];
-    for($i = 0; $i <= 2; $i++) {
+    for ($i = 0; $i <= 2; $i++) {
         $num = rand(1, 15);
         $array[] = [$num];
         if ($num % 2 === 0) {
@@ -19,8 +19,7 @@ function operacion()
         } else {
             $arrayRand[] = ['no'];
         }
-    
-}
+    }
     $logic = logic($array, $question, $arrayRand);
     return $logic;
 }
