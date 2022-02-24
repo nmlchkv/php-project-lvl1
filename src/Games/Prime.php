9372d874dcb13fa40be29b991702c0cf1d28ab0d
itaopro <malchikov.ne@gmail.com>
2022-02-24 13:01:37 +0300

    update

--- src/Games/Prime.php
+++ src/Games/Prime.php
@@ -8,49 +8,18 @@ use function src\engine\logic;
 
 function operacion()
 {
-    $question = ('What number is missing in the progression?');
+    $question = ('Answer "yes" if given number is prime. Otherwise answer "no".');
     $arrayRand = [];
-    $result1 = [];
-    for ($i = 0; $i <= 100; $i++) {
-        if ($i % 6 === 0) {
-            $result1[] = $i; 
-        } if (count($result1) === 10) {
-            break;
-          }
+    $array = [];
+    for ($i = 0; $i <= 2; $i++) {
+        $num = rand(1, 100);
+        $array[] = [$num];
+        if (gmp_prob_prime($num) === 2) {
+            $arrayRand[] = ['yes'];
+        } else {
+            $arrayRand[] = ['no'];
+        }
     }
-    $str1 = implode(' ', $result1);
-    $count1 = count($result1) - 1;
-    $rand1 = rand(0, $count1);
-    $randStr1 = $result1[$rand1];
-    $number1 = str_replace($randStr1, '..', $str1);
-    $result2 = [];
-    for ($i = 0; $i <= 100; $i++) {
-        if ($i % 7 === 0) {
-            $result2[] = $i;
-        } if (count($result2) === 10) {
-            break;
-          }
-    }
-    $str2 = implode(' ', $result2);
-    $count2 = count($result2) - 1;
-    $rand2 = rand(0, $count2);
-    $randStr2 = $result2[$rand2];
-    $number2 = str_replace($randStr2, '..', $str2);
-    $result3 = [];
-    for ($i = 0; $i <= 100; $i++) {
-        if ($i % 8 === 0) {
-            $result3[] = $i;
-        } if (count($result3) === 10) {
-            break;
-          }
-    }
-    $str3 = implode(' ', $result3);
-    $count3 = count($result3) - 1;
-    $rand3 = rand(0, $count3);
-    $randStr3 = $result3[$rand3];
-    $number3 = str_replace($randStr3, '..', $str3);
-    $array = [[$number1], [$number2], [$number3]];
-    $arrayRand = [[$randStr1], [$randStr2], [$randStr3]];
     $logic = logic($array, $question, $arrayRand);
     return $logic;
 }
