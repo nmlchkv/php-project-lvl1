79e8f15ecdc0e29872a23e67c4b8d381b6c29a77
itaopro <malchikov.ne@gmail.com>
2022-02-25 11:36:51 +0300

    u

--- src/Games/Gcd.php
+++ src/Games/Gcd.php
@@ -15,7 +15,7 @@ function operacion()
         $num1 = rand(1, 10);
         $num2 = rand(1, 10);
         $array[] = ["$num1 $num2"];
-        $num = gmp_gcd($num1, $num2);
+        $num = gcd($num1, $num2);
         $arrayRand[] = [$num];
     }
     $logic = logic($array, $question, $arrayRand);
