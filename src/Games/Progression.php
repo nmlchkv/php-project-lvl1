9b72a1b758776f0ae6d9b8d375981ae358ea0e78
Nikolai Malchikov <malchikov.ne@gmail.com>
2022-02-25 14:32:54 +0300

    update

--- src/Games/Progression.php
+++ src/Games/Progression.php
@@ -10,47 +10,29 @@ function operacion()
 {
     $question = ('What number is missing in the progression?');
     $arrayRand = [];
-    $result1 = [];
-    for ($i = 0; $i <= 100; $i++) {
-        if ($i % 6 === 0) {
-            $result1[] = $i;
-        } if (count($result1) === 10) {
-            break;
-        }
+    $result = [];
+    for ($i = 0; $i <= 2; $i++) {
+        $arr = progression ($result);
+        $str = implode(' ', $arr);
+        $count = count($arr) - 1;
+        $index = rand(0, $count);
+        $randStr = $arr[$index];
+        $number = str_replace([$randStr], '..', $str);
+        $array[] = [$number];
+        $arrayRand[] = [$randStr];
     }
-    $str1 = implode(' ', $result1);
-    $count1 = count($result1) - 1;
-    $rand1 = rand(0, $count1);
-    $randStr1 = $result1[$rand1];
-    $number1 = str_replace([$randStr1], '..', $str1);
-    $result2 = [];
-    for ($i = 0; $i <= 100; $i++) {
-        if ($i % 7 === 0) {
-            $result2[] = $i;
-        } if (count($result2) === 10) {
-            break;
-        }
-    }
-    $str2 = implode(' ', $result2);
-    $count2 = count($result2) - 1;
-    $rand2 = rand(0, $count2);
-    $randStr2 = $result2[$rand2];
-    $number2 = str_replace([$randStr2], '..', $str2);
-    $result3 = [];
-    for ($i = 0; $i <= 100; $i++) {
-        if ($i % 8 === 0) {
-            $result3[] = $i;
-        } if (count($result3) === 10) {
-            break;
-        }
-    }
-    $str3 = implode(' ', $result3);
-    $count3 = count($result3) - 1;
-    $rand3 = rand(0, $count3);
-    $randStr3 = $result3[$rand3];
-    $number3 = str_replace([$randStr3], '..', $str3);
-    $array = [[$number1], [$number2], [$number3]];
-    $arrayRand = [[$randStr1], [$randStr2], [$randStr3]];
     $logic = logic($array, $question, $arrayRand);
     return $logic;
+}
+function progression(array $result1) 
+{
+    $randNumber = rand(100, 200);
+    $randDiv = rand(6, 8);
+     for ($i = 0; $i <= $randNumber; $i++) {
+        if ($i % $randDiv === 0) {
+            $result1[] = $i;
+        } if (count($result1) === 10) {
+            return $result1;
+        }
+    }
 }
