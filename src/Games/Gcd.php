c40ad92cce3d88d73963d358bdaeb727295aa1c9
itaopro <malchikov.ne@gmail.com>
2022-02-25 11:36:18 +0300

    update

--- src/Games/Gcd.php
+++ src/Games/Gcd.php
@@ -21,3 +21,10 @@ function operacion()
     $logic = logic($array, $question, $arrayRand);
     return $logic;
 }
+function gcd($num1, $num2) {
+    if($num2 > 0) {
+        return gcd($num2, $num1 % $num2);
+    } else {
+        return abs($num1);
+    }
+}
