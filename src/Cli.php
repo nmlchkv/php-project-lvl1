6fce13f919b8d05e6b7fdfe6c5a18a659ea18ff1
itaopro <malchikov.ne@gmail.com>
2022-03-03 09:44:35 +0300

    update

--- src/Cli.php
+++ src/Cli.php
@@ -5,7 +5,7 @@ namespace Src\Cli;
 use function Cli\Line;
 use function Cli\Prompt;
 
-function calculation()
+function runGame();
 {
     Line('Welcome to the Brain Games!');
     $name = Prompt('May I have your name?');
