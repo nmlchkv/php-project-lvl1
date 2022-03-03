380ad117f50a42d8902f5022379d3e78b71f0015
itaopro <malchikov.ne@gmail.com>
2022-03-03 10:06:46 +0300

    update

--- src/Engine.php
+++ src/Engine.php
@@ -4,13 +4,14 @@ namespace Src\Engine;
 
 use function Cli\Line;
 use function Cli\Prompt;
+use function Src\Games\Calc;
 
 function execution(array $result, string $question, array $data)
 {
     Line('Welcome to the Brain Games!');
     $name = Prompt('May I have your name?');
     Line("Hello, %s!", $name);
-    Line($question);
+    Calc($question);
     for ($i = 0; $i <= count($result) - 1; $i++) {
         $str = implode(' ', $result[$i]);
         $strRand = implode(' ', $data[$i]);
