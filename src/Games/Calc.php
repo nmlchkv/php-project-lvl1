8f9169869d993c013a54e1409d471dc3ef18e7fd
itaopro <malchikov.ne@gmail.com>
2022-03-05 12:05:00 +0300

    update

--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -7,25 +7,26 @@ use function Src\Engine\execution;
 use const Src\Engine\ROUNDS_COUNT;
 
 const DESCRIPTION_CALC = ('What is the result of the expression?');
-const OPERATORS = ['-', '+', '*'];
 function runGame()
 {
     $correctAnswer = [];
     $question = [];
     $startQuestion = DESCRIPTION_CALC;
     for ($i = 0; $i <= ROUNDS_COUNT; $i++) {
+        $operators = ['-', '+', '*'];
+        $keyOperator = rand(0, 2);
+        $operator = $operators[$keyOperator];
         $num1 = rand(1, 10);
         $num2 = rand(1, 10);
-        $question[] = [$num1 . ' ' . OPERATORS . ' ' . $num2];
-        $correctAnswer[] = [calculate($num1, $num2)];
+        $question[] = [$num1 . ' ' . $operator . ' ' . $num2];
+        $correctAnswer[] = [calculate($operator, $num1, $num2)];
     }
     $logic = execution($question, $startQuestion, $correctAnswer);
     return $logic;
 }
 
-function calculate(int $num1, int $num2)
+function calculate(string $operator, int $num1, int $num2)
 {
-    $operator = array_rand(OPERATORS);
     switch ($operator) {
         case '+':
             return ($num1 + $num2);
