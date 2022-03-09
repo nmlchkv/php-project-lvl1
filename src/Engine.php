5077e992623a7fd5ff0e16bee525f269154820ee
Nikolay Malchikov <n.malchikov@srv1.sbcmedia.ru>
2022-03-09 14:11:45 +0300

    update

--- src/Engine.php
+++ src/Engine.php
@@ -5,14 +5,14 @@ namespace Src\Engine;
 use function cli\line;
 use function cli\prompt;
 
-const ROUNDS_COUNT = 2;
+const ROUNDS_COUNT = 3;
 function startGame(string $startQuestion, array $questionsAnswers)
 {
     line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
     line($startQuestion);
-    for ($i = 0; $i <= ROUNDS_COUNT; $i++) {
+    for ($i = 1; $i <= ROUNDS_COUNT; $i++) {
         $question = $questionsAnswers[0];
         $correctAnswer = $questionsAnswers[1];
         $roundQuestion = implode(' ', $question[$i]);
