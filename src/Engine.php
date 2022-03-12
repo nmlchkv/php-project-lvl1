8b5a61455dd3c6e8d2a542225f9621ea87151cb4
itaopro <48715986+itaopro@users.noreply.github.com>
2022-03-12 14:37:43 +0300

    Update Engine.php

--- src/Engine.php
+++ src/Engine.php
@@ -15,8 +15,9 @@ function startGame(string $startQuestion, array $questionsAnswers)
     for ($i = 1; $i <= ROUNDS_COUNT; $i++) {
         $question = $questionsAnswers[0];
         $correctAnswer = $questionsAnswers[1];
+        $roundQuestion = implode(' ', $question[$i]);
         $roundAnswer = implode(' ', $correctAnswer[$i]);
-        line('Question: ' . $question[$i]);
+        line('Question: ' . $roundQuestion);
         $answer = prompt('Your answer');
         if ($roundAnswer == $answer) {
             line('Correct!');
