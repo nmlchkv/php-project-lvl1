e7e2b3902f25df52308b35b0f04defb1adfd3399
itaopro <malchikov.ne@gmail.com>
2022-03-12 20:54:03 +0300

    update

--- src/Games/Even.php
+++ src/Games/Even.php
@@ -14,7 +14,7 @@ function runGame()
     for ($i = 0; $i <= ROUNDS_COUNT; $i++) {
         $num = rand(1, 15);
         $question[] = [$num];
-        $correctAnswer[] = isEven($num);
+        $correctAnswer[] = [isEven($num)];
     }
     $questionsAnswers = [$question, $correctAnswer];
     return (startGame(DESCRIPTION, $questionsAnswers));
