64a713ae90765fcce57c8fa62fbdb6c845f91f04
itaopro <malchikov.ne@gmail.com>
2022-03-12 19:39:04 +0300

    update gcd str18 toCheckDivider

--- src/Games/Gcd.php
+++ src/Games/Gcd.php
@@ -15,7 +15,7 @@ function runGame()
         $num1 = rand(1, 10);
         $num2 = rand(1, 10);
         $question[] = ["$num1 $num2"];
-        $num = gcd($num1, $num2);
+        $num = toCheckDivider($num1, $num2);
         $correctAnswer[] = [$num];
     }
     $questionsAnswers = [$question, $correctAnswer];
