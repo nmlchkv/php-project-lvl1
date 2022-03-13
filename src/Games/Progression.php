181c2a8878caf23a3284370cc6990904f8371988
itaopro <malchikov.ne@gmail.com>
2022-03-13 22:17:29 +0300

    update

--- src/Games/Progression.php
+++ src/Games/Progression.php
@@ -28,9 +28,8 @@ function makeProgression(): array
 {
     $result = [];
     $randNumber = rand(2, 4);
-    for ($i = 0; $i <= 20; $i + $randNumber) {
+    for ($i = 0; $i <= 20; $i += $randNumber) {
         $result[] = $i;
-        return $result;
     }
-    
+    return $result;
 }
